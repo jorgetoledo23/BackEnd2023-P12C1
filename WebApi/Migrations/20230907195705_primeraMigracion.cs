74d75631fe892009868a2ffcdfc3e7a996121261
Jorge <jorge.toledo.6@hotmail.com>
2023-09-07 17:23:12 -0300

    EFCore

---
+++ WebApi/Migrations/20230907195705_primeraMigracion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class primeraMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TblDepartamentos",
+                columns: table => new
+                {
+                    Cod_Dpto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TblDepartamentos", x => x.Cod_Dpto);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TblTrabajadores",
+                columns: table => new
+                {
+                    Rut = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comuna = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cod_Dpto = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TblTrabajadores", x => x.Rut);
+                    table.ForeignKey(
+                        name: "FK_TblTrabajadores_TblDepartamentos_Cod_Dpto",
+                        column: x => x.Cod_Dpto,
+                        principalTable: "TblDepartamentos",
+                        principalColumn: "Cod_Dpto",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TblTrabajadores_Cod_Dpto",
+                table: "TblTrabajadores",
+                column: "Cod_Dpto");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TblTrabajadores");
+
+            migrationBuilder.DropTable(
+                name: "TblDepartamentos");
+        }
+    }
+}
---
+++ WebApi/Model/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Model
+{
+    //Class de Conexion a la BD
+    public class AppDbContext : DbContext
+    {
+        //Tablas de la BD
+        public DbSet<Departamento> TblDepartamentos { get; set; }
+        public DbSet<Trabajador> TblTrabajadores { get; set; }
+        // --- Fin Tablas --
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ApiWeb2023P12C1;Integrated Security=True;");
+        }
+    }
+
+    public class Departamento
+    {
+        [Key]
+        public int Cod_Dpto { get; set; } //PK
+
+        [StringLength(50, ErrorMessage ="Exede Maximo de Caracteres")]
+        //[DataType(DataType.Password)]
+        [Required]
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+
+    public class Trabajador
+    {
+        [Key]
+        public string Rut { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Comuna { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+
+        //Clave Foranea
+        [ForeignKey("Departamento")]
+        public int Cod_Dpto { get; set; }
+        public Departamento Departamento { get; set; }
+
+
+    }
+
+
+}
