fed26ae4879ded549fb2f80cee5ecd61f49e2ede
Jorge <jorge.toledo.6@hotmail.com>
2023-10-05 16:22:11 -0300

    asd

---
+++ WebApi/Controllers/TrabajadorController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [Route("api/v3/[controller]")]
+    [ApiController]
+    public class TrabajadorController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TrabajadorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //TODO: Delete
+        //TODO: Update
+        //TODO: GetByName
+        //TODO: GetByComuna
+        //TODO: GetByRut
+        //TODO: GetByCargo
+
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IActionResult> getAll()
+        {
+            var allTrabs = await _context.TblTrabajadores.ToListAsync();
+            return Ok(allTrabs);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> addTrabj(TrabajadorDTO Tdto)
+        {
+            var T = new Trabajador()
+            {
+                Rut = Tdto.Rut,
+                Nombres = Tdto.Nombres,
+                Apellidos = Tdto.Apellidos,
+                Cod_Dpto = Tdto.Cod_Dpto,
+                Correo = Tdto.Correo,
+                Telefono = Tdto.Telefono,
+                Direccion = Tdto.Direccion,
+                Comuna = Tdto.Comuna,
+            };
+            _context.TblTrabajadores.Add(T);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ WebApi/Model/TrabajadorDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Model
+{
+    public class TrabajadorDTO
+    {
+        public string Rut { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Comuna { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public int Cod_Dpto { get; set; }
+    }
+}
