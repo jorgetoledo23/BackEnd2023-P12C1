c9279a546b7ec000ab113d3e3cf6201572cbfd94
Jorge <jorge.toledo.6@hotmail.com>
2023-11-16 18:09:51 -0300

    Web App

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly AppDbContext _context;
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> LoginIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginIn(LoginViewModel lvm)
+        {
+            var usuarios = _context.Usuarios.ToList();
+            if (usuarios.Count == 0)
+            {
+                var superAdmin = new Usuario();
+                superAdmin.isBlock = false;
+                superAdmin.Name = "Admin";
+                superAdmin.Email = "admin@admin.cl";
+                superAdmin.Rol = "SuperAdministrador";
+                CreatePasswordHash("123456", out byte[] hash, out byte[] salt);
+                superAdmin.PasswordHash = hash;
+                superAdmin.PasswordSalt = salt;
+                _context.Add(superAdmin);
+                await _context.SaveChangesAsync();
+            }
+
+            var user = _context.Usuarios.FirstOrDefault(u => u.Email.Equals(lvm.Email));
+            if(user == null)
+            {
+                ModelState.AddModelError("", "Email no encontrado");
+                return View();
+            }
+            if(VerifyPassword(lvm.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                var claims = new List<Claim>(){
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Role, user.Rol)
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, 
+                    new AuthenticationProperties { IsPersistent = true });
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Contraseña Incorrecta");
+                return View();
+            }
+
+            return View();
+
+        }
+
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash,
+            out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPassword(string password, byte[] passwordHash,
+            byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
---
+++ WebApp/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AppWeb2023P12C1;Integrated Security=True;");
+        }
+    }
+
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Rol { get; set; }
+        public bool isBlock { get; set; }
+    }
+}
